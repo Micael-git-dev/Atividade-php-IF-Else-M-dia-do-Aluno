6a52b249c5d6064301d3f83261679ecb5624edc4
Micael-git-dev <micaels3nac@gmail.com>
2024-08-27 20:57:58 -0300

    Update atividademedia.php

--- atividademedia.php
+++ atividademedia.php
@@ -1,31 +1,36 @@
 <?php     
-            $nota1 = 10;
-            $nota2 = 10;
-            $nota3 = 10;
+            $nota1 = 0;
+            $nota2 = 0;
+            $nota3 = 0;
             $nota4 = 10;
 
             $resultado = ($nota1 + $nota2 + $nota3 + $nota4) / 4;
+            $mensagem = '';
             
             echo "Suas notas foram:"."<br>";
             echo "1° nota: ".$nota1."<br>";
             echo "2° nota: ".$nota2."<br>";
             echo "3° nota: ".$nota3."<br>";
             echo "4° nota: ".$nota4."<br>";
-            echo "------------- <br>";
+            echo "--------------<br>";
             echo "Média: ";
             echo $resultado;
-            echo "<br>-------------<br>";
+            echo "<br>--------------<br>";
 
             if ($resultado >=7 && $resultado !=10) {
-                echo "O aluno foi aprovado!";
+                $mensagem = 'O aluno foi aprovado.';
+                echo $mensagem;
+                /*echo "O aluno foi aprovado!";*/
             }
-            elseif ($resultado == 0){
+            else if ($resultado == 0){
                 echo "Estude mais, você não acertou nada.";
             }
-            elseif ($resultado == 10) {
+            else if ($resultado == 10) {
                 echo "Parabéns! Aprovado com nota máxima.";
             }
             else  {
-                echo "O aluno foi reprovado";
+                $mensagem = 'O aluno foi reprovado.';
+                echo $mensagem;
+                /*echo "O aluno foi reprovado";*/
             }
-?> 
+?>     
